befebf12525529aa7a26496475a246f2a82f67d7
tortoiselala <tortoiselala@gmail.com>
2019-07-17 23:21:29 +0800

    update

---
+++ src/medium/PrintBinaryTree.java
@@ -0,0 +1,25 @@
+package medium;
+
+import entity.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/print-binary-tree/
+ * @author tortoiselala
+ */
+public class PrintBinaryTree {
+    public List<List<String>> printTree(TreeNode root) {
+        List<List<String>> re = new ArrayList<>();
+
+    }
+
+    private void helper(TreeNode root, List<List<String>> re, int index, int level){
+
+        if(root.left != null){
+            
+        }
+    }
+}
