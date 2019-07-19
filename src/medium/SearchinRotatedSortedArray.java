5ea8418349855e9f2bd875a08bfa819b1628ce73
yangminwu(吴阳民) <yangminwu@futunn.com>
2019-07-19 21:27:49 +0800

    update

--- src/medium/SearchinRotatedSortedArray.java
+++ src/medium/SearchinRotatedSortedArray.java
@@ -1,11 +1,58 @@
 package medium;
 
+import java.util.Arrays;
+
 /**
  * Created by tortoiselala on 2019-05-29
  * https://leetcode.com/problems/search-in-rotated-sorted-array/
  */
 public class SearchinRotatedSortedArray {
     public int search(int[] nums, int target) {
-return 0;
+        if(nums == null || nums.length == 0){
+            return -1;
+        }
+        return search(nums, target, 0, nums.length);
+    }
+
+    public int search(int[] nums, int target, int l, int r){
+        if(l + 1 >= r){
+            if(l >= r){
+                return -1;
+            }
+            return nums[l] == target ? l : -1;
+        }
+        int mid = l + (r - l) / 2;
+
+        if(nums[mid] == target){
+            return mid;
+        }
+
+        // 左右都有序
+        if(nums[l] <= nums[mid] && nums[mid] <= nums[r - 1]){
+            if(nums[mid] < target){
+                return search(nums, target, mid + 1, r);
+            }else{
+                return search(nums, target, l, mid);
+            }
+        }
+
+        // 左侧有序
+        if(nums[mid] > nums[l]){
+            if(nums[0] < target && target < nums[mid]){ // 在有序的一侧
+                return search(nums, target, 0, mid - 1);
+            }else{
+                return search(nums, target, mid + 1, r);
+            }
+        }
+
+        // 右侧有序
+        if(nums[mid] < nums[r - 1]){
+            if(nums[mid] < target && target < nums[r - 1]){
+                return search(nums, target, mid + 1, r);
+            }else{
+                return search(nums, target, l, mid);
+            }
+        }
+        return -1;
     }
 }
--- test/medium/SearchinRotatedSortedArrayTest.java
+++ test/medium/SearchinRotatedSortedArrayTest.java
@@ -11,14 +11,8 @@ public class SearchinRotatedSortedArrayTest {
 
     @Test
     public void search() {
-//        System.out.println(
-//                new SearchinRotatedSortedArray().search(
-//                        new int[]{5, 1, 3}, 5));
-//        int[] array = new int[]{4, 5, 6, 7, 8, 0, 1, 2, 3};
-//        for(int e : array){
-//            System.out.println(new SearchinRotatedSortedArray().search(array, e));
-//        }
-        int[] array2 = new int[]{1, 3};
-        System.out.println(new SearchinRotatedSortedArray().search(array2, 3));
+        System.out.println(new SearchinRotatedSortedArray().search(new int[]{1, 3}, 0));
+
     }
+
 }
