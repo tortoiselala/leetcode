c4716bbf25939c3fad86dadb7ac3cffff5dcb577
yangminwu(吴阳民) <yangminwu@futunn.com>
2019-08-07 09:27:03 +0800

    update

--- src/medium/ContainsDuplicateIII.java
+++ src/medium/ContainsDuplicateIII.java
@@ -2,6 +2,13 @@ package medium;
 
 public class ContainsDuplicateIII {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length && j <= i + k; j++) {
+                if(Math.abs((long)nums[i] - (long)nums[j]) <= t){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
